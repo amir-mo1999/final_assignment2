121efef75bb3e643e6aa498f4c6b31b9c242265c
amir-mo1999 <amir.mohammad@hotmail.de>
2025-11-18 19:06:44 +0100

    fix settings

--- agent/config/settings.py
+++ agent/config/settings.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings
 from typing import Optional
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
 
 
 class Settings(BaseSettings):
